82f384ba49df8a7babba8ed9b5ad9069d4786b0d
Frank <franklinewax@gmail.com>
2020-06-15 01:21:30 +0300

    Adding Likes and disLikes in MyQuotes class

--- src/app/my-quote.ts
+++ src/app/my-quote.ts
@@ -1,5 +1,5 @@
 export class MyQuote {
-    constructor(public id:number, public author:string, public quote:string, public publisher:string, public voteCount:number,public dateCreated:Date ){
+    constructor(public id:number, public author:string, public quote:string, public publisher:string, public likes: number, public disLikes: number,public dateCreated:Date ){
 
     }
 }
