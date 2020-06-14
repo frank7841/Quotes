cf503909fa56ab47aefaa0d99bc0a77299b6afb7
Frank <franklinewax@gmail.com>
2020-06-14 10:40:51 +0300

    created myquotes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyquotesComponent } from './myquotes/myquotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyquotesComponent
   ],
   imports: [
     BrowserModule,
