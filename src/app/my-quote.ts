5335f22c83133a8fc50a02fa1fc67ff4919919f8
Frank <franklinewax@gmail.com>
2020-06-14 08:56:32 +0300

    Creating and defining myQuote class

---
+++ src/app/my-quote.ts
@@ -0,0 +1,5 @@
+export class MyQuote {
+    constructor(public id:number, public author:string, public quote:string, public publisher:string, public voteCount:number,public dateCreated:Date ){
+
+    }
+}
