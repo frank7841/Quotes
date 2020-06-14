4a0824ac6c8d30a3f9945d2deac72b85220c61ff
Frank <franklinewax@gmail.com>
2020-06-14 11:09:32 +0300

    creating quote form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyquotesComponent } from './myquotes/myquotes.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyquotesComponent
+    MyquotesComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
