19b0bb44453ede05a88a789746668740d80865c6
Frank <franklinewax@gmail.com>
2020-06-15 03:14:03 +0300

    diploying to gh-pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
-
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,8 +21,6 @@ import { HighlightDirective } from './highlight.directive';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    FontAwesomeModule
-
   ],
   providers: [],
   bootstrap: [AppComponent]
