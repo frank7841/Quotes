6831ac8edf84a8ba34746a0e868ce2d0651f4019
Frank <franklinewax@gmail.com>
2020-06-16 09:33:51 +0300

    finalising on the project

--- src/app/date-passed.pipe.ts
+++ src/app/date-passed.pipe.ts
@@ -1,24 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Pipe({
   name: 'datePassed'
 })
 export class DatePassedPipe implements PipeTransform {
 
-  transform(value: any): number{
+  transform(value: any): any{
     let today:Date = new Date();
     let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
     var dateDifference =Math.abs(todayWithNoTime - value)
     const secondsInADay= 86400;
+
     var dateDifferenceSeconds=dateDifference*0.001; 
     var dateCounter = dateDifferenceSeconds/secondsInADay;
+    var datePassed = Math.trunc(dateCounter)
 
-
-
-  if (dateCounter >= 1 && todayWithNoTime > value){
-        return dateCounter
+    if (dateCounter >= 1 && todayWithNoTime > value){
+        return datePassed;
     }else{
-      return 0;
+        return 0;
     }
   }
 
